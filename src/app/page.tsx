2f93203ce6bdb054c53bd88efe4f40ee7c95f731
felixDev22 <felixnzomo@gmail.com>
2023-08-20 23:55:18 +0300

    Create contact section, import to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { About } from "./about/about";
+import { Contact } from "./contact/contact";
 import { Gallery } from "./gallery/gallery";
 import { Hero } from "./hero/hero";
 
 export default function Home() {
   return (
-<main>
-      <Hero/>
+    <main>
+      <Hero />
       <About />
-      <Gallery/>
+      <Gallery />
+      <Contact />
     </main>
   );
 };
