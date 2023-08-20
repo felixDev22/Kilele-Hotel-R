1e9b2e0b4a7ea7fabcda9d8b936f932514814133
felixDev22 <felixnzomo@gmail.com>
2023-08-20 23:39:44 +0300

    Set-up the page sections

---
+++ src/app/gallery/gallery.tsx
@@ -0,0 +1,8 @@
+
+export const Gallery = () => {
+  return (
+    <section>
+      <div className="bg-[#32a895] w-full h-40">This is the Gallery page</div>
+    </section>
+  );
+}
---
+++ src/app/hero/hero.tsx
@@ -0,0 +1,7 @@
+export const Hero = () => {
+  return (
+    <section>
+      <div className="bg-[#44a5dd] w-full h-40">This is the hero page</div>
+    </section>
+  );
+};
