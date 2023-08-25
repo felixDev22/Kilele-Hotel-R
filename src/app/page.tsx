72536c4c36c2a8efd52eb82d64cab7d88dcc98d2
felixDev22 <felixnzomo@gmail.com>
2023-08-25 06:30:26 +0300

    Add fontawesome CDN to the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
 
+import About from "./about/page";
 import { Contact } from "./contact/contact";
-import { Gallery } from "./gallery/gallery";
 import { Hero } from "./hero/hero";
+import './globals.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main className="overflow-y-scroll">
-      <Hero />
-  
-      <Gallery />
-      <Contact />
-    </main>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+          integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
+      <main className="container">
+        <Hero />
+        <About />
+
+        <Contact />
+      </main>
+    </>
   );
 }
