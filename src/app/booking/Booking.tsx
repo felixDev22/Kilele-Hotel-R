cebb0650de0700ecfb91b211bc017368eb780be2
felixDev22 <felixnzomo@gmail.com>
2023-08-25 20:58:31 +0300

    Add margin right for mobile

--- src/app/booking/Booking.tsx
+++ src/app/booking/Booking.tsx
@@ -3,7 +3,7 @@
 export const Booking = () => {
   return (
     <>
-      <div className="flex-col  w-80vw p-4 h-40">
+      <div className="flex-col w-80vw p-4 mr-6">
       
         <div className="form grid">
           <form className="flex-col md:flex-row lg:flex-row space-y-6 md:space-y-0 lg:space-y-0 md:space-x-2 lg:space-x-2">
