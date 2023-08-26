d160041a895c8d923e1ac1f3ac0cae8352e77107
felixDev22 <felixnzomo@gmail.com>
2023-08-27 00:06:56 +0300

    Fine tune the setttings for desk and mobile

--- src/app/booking/Booking.tsx
+++ src/app/booking/Booking.tsx
@@ -3,9 +3,9 @@
 export const Booking = () => {
   return (
     <>
-      <div className="flex-col w-80vw p-4 mr-6">
+      <div className="w-80vw p-4 mr-6">
         <div className="form grid">
-          <form className="flex-col md:flex-row lg:flex-row space-y-6 md:space-y-0 lg:space-y-0 md:space-x-2 lg:space-x-2">
+          <form className="flex-col md:flex-row lg:flex-row md:space-y-0 lg:space-y-0 md:space-x-2 lg:space-x-2">
             <input type="date" placeholder="Arrival Date" />
             <input type="date" placeholder="Departure Date" />
             <input type="number" placeholder="Adult" className="input2" />
@@ -14,7 +14,7 @@ export const Booking = () => {
             <button
               type="submit"
               value="CHECK AVAILABILITY"
-              className="btn bg-[#81642d] text-white lg:ml-6 md:text-base lg:text-lg">
+              className="btn mt-6 text-l bg-[#81642d] text-white lg:ml-6 md:text-base lg:text-lg">
               CHECK AVAILABILITY
             </button>
           </form>
