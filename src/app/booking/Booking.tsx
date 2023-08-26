7718e18dd5082421e61dd60f154c87c010eab7c5
felixDev22 <felixnzomo@gmail.com>
2023-08-26 20:50:25 +0300

    Add a class to align the input box

--- src/app/booking/Booking.tsx
+++ src/app/booking/Booking.tsx
@@ -4,29 +4,13 @@ export const Booking = () => {
   return (
     <>
       <div className="flex-col w-80vw p-4 mr-6">
-      
         <div className="form grid">
           <form className="flex-col md:flex-row lg:flex-row space-y-6 md:space-y-0 lg:space-y-0 md:space-x-2 lg:space-x-2">
-            <input
-              type="date"
-              placeholder="Arrival Date"
-            />
-            <input
-              type="date"
-              placeholder="Departure Date"
-            />
-            <input
-              type="number"
-              placeholder="Adult"
-            />
-            <input
-              type="number"
-              placeholder="Children"
-            />
-            <input
-              type="number"
-              placeholder="Room"
-            />
+            <input type="date" placeholder="Arrival Date" />
+            <input type="date" placeholder="Departure Date" />
+            <input type="number" placeholder="Adult" className="input2" />
+            <input type="number" placeholder="Children" className="input2" />
+            <input type="number" placeholder="Room" className="input2" />
             <button
               type="submit"
               value="CHECK AVAILABILITY"
