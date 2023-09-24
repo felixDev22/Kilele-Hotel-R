010e2f5666a7a271aa31220132a295beeaf767d4
felixDev22 <felixnzomo@gmail.com>
2023-09-24 22:23:06 +0300

    Import arrow for direction, Add styling

--- src/app/carousel/Carousel.js
+++ src/app/carousel/Carousel.js
@@ -1,12 +1,18 @@
+
+import { FiArrowLeftCircle, FiArrowRightCircle } from 'react-icons/fi';
+
 export default function Carousel({children: slides}) {
   return (
-    <div className="flex overflow-hidden relative">
-      {slides}
-    </div>
-    <div>
-      <button>
-        <ChevronLeft />
-      </button>
-    </div>
+    <>
+      <div className="flex overflow-hidden relative px-4">{slides}</div>
+      <div>
+        <button className="absolute top-1/2 left-8">
+          <FiArrowLeftCircle size={30} />
+        </button>
+        <button className="absolute top-1/2 right-8">
+          <FiArrowRightCircle size={30} />
+        </button>
+      </div>
+    </>
   );
 }
