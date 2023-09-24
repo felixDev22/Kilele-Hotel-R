754c6fbcd83cbfc654eca44634737ac066c9076c
felixDev22 <felixnzomo@gmail.com>
2023-09-24 20:52:01 +0300

    Set up gallery slide images function

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -1,8 +1,23 @@
+import Image from 'next/image';
+import Carousel from '../carousel/Carousel';
+
+const slides = [
+  '/assets/zebra.jpg',
+  '/assets/zebra.jpg',
+  '/assets/zebra.jpg',
+  '/assets/zebra.jpg',
+];
 
 export default function Gallery() {
   return (
-    <div className="flex items-center justify-center h-screen bg-green-600">
-      <h1 className="text-white">This is the Gallery page</h1>
+    <div className="bg-[#3a2d2d] h-screen">
+      <Carousel autoSlide={true} />
+      {slides.map((slide, index) => (
+        <div key={index} className='lg:w-[80vw] lg:h-[80vh]'>
+          <Image src={slide} layout="fill" objectFit="cover" alt="gallery" />
+        </div>
+      ))}
+      <Carousel />
     </div>
   );
-};
+}
