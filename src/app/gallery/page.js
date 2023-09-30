64f6e59a57502a0e686100f1322d50837d7142b8
felixDev22 <felixnzomo@gmail.com>
2023-09-30 21:52:56 +0300

    Import Carousel, add use client

---
+++ src/app/gallery/page.js
@@ -0,0 +1,11 @@
+
+"use client"
+import Carousel from '../carousel/Carousel';
+
+export default function Gallery() {
+  return (
+    <div className='overflow-hidden'>
+      <Carousel />
+    </div>
+  );
+}
