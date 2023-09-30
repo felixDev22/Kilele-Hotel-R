b6daff14e9b8bc77f7d45701a2ec0c6ceecc35a0
felixDev22 <felixnzomo@gmail.com>
2023-09-30 21:53:58 +0300

    Adjust border weight

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="border-white  border-b-2 w-full fixed top-0 left-0 px-6 py-2 lg:px-16 lg:py-2 z-50">
+    <header className="border-white  border-b w-full fixed top-0 left-0 px-6 py-2 lg:px-16 lg:py-2 z-50">
       <div className="flex items-center justify-between space-x-2 md:space-x-10">
         <div>
           <Link href="/">
@@ -36,7 +36,7 @@ export const Header = () => {
             </Link>
           </li>
         </ul>
-        <div className="border-white border-l-2">
+        <div className="border-white border-l">
           <button
             className="btn text-white hover:bg-[#81642d] "
             aria-label="Book Now">
