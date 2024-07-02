d2fc89b2523c6be0c8c140c48c76226aefac4af5
felixDev22 <felixnzomo@gmail.com>
2024-07-02 23:39:26 +0300

    Update booking form, add error handling

--- src/app/booking/Booking.tsx
+++ src/app/booking/Booking.tsx
@@ -1,4 +1,5 @@
-import React, { Children, useState } from 'react';
+'use client';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 export const Booking = () => {
@@ -10,56 +11,103 @@ export const Booking = () => {
     room: 0,
   });
 
-  const handleChange = (e: { target: { name: any; value: any; }; }) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
-  };
-
   const [availabilityMessage, setAvailabilityMessage] = useState('');
   const [showPopup, setShowPopup] = useState(false);
 
+  const handleChange = (e: { target: { name: any; value: any } }) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const handleCheckAvailability = async (e) => {
     e.preventDefault();
-    try{
-      const response = await axios.post('http://localhost:3000/api/v1/checkAvailability', formData);
-      if (response.data.available){
+    console.log("this is", formData),
+    try {
+      const response = await axios.post(
+        'http://localhost:3000/api/v1/check_availability',
+        formData,
+        
+      );
+      if (response.data.available) {
         setAvailabilityMessage('Book Now');
-      }else{
+      } else {
         setAvailabilityMessage('We currently are fully booked');
       }
       setShowPopup(true);
-  }catch (error){
-    console.error('Error checking availability:', error);
-    setAvailabilityMessage('Error checking availability');
-    setShowPopup(true)
-  }
-
-  const closePopup = () => {
-    setShowPopup(false)
+    } catch (error) {
+      console.error('Error checking availability:', error);
+      setAvailabilityMessage('Error checking availability');
+      setShowPopup(true);
+    }
   };
 
+  const closePopup = () => {
+    setShowPopup(false);
+  };
 
   return (
     <>
       <div className="w-80vw p-4 mr-6">
         <div className="form grid">
-          <form className="flex-col md:flex-row lg:flex-row md:space-y-0 lg:space-y-0 md:space-x-2 lg:space-x-2">
-            <input type="date" placeholder="Arrival Date" name='date_of_arrival' value={formData.date_of_arrival} onChange={handleChange}/>
-            <input type="date" placeholder="Departure Date" name='date_of_departure' value={formData.date_of_departure} onChange={handleChange}/>
-            <input type="number" placeholder="Adult" className="input2"  name='adults' value={formData.adults} onChange={handleChange}/>
-            <input type="number" placeholder="Children" className="input2" name='children' value={formData.children} onChange={handleChange}/>
-            <input type="number" placeholder="Room" className="input2" name='room' value={formData.room} onChange={handleChange}/>
+          <form
+            className="flex-col md:flex-row lg:flex-row md:space-y-0 lg:space-y-0 md:space-x-2 lg:space-x-2"
+            onSubmit={handleCheckAvailability}>
+            <input
+              type="date"
+              placeholder="Arrival Date"
+              name="date_of_arrival"
+              value={formData.date_of_arrival}
+              onChange={handleChange}
+            />
+            <input
+              type="date"
+              placeholder="Departure Date"
+              name="date_of_departure"
+              value={formData.date_of_departure}
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              placeholder="Adult"
+              className="input2"
+              name="adults"
+              value={formData.adults}
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              placeholder="Children"
+              className="input2"
+              name="children"
+              value={formData.children}
+              onChange={handleChange}
+            />
+            <input
+              type="number"
+              placeholder="Room"
+              className="input2"
+              name="room"
+              value={formData.room}
+              onChange={handleChange}
+            />
             <button
               type="submit"
-              value="CHECK AVAILABILITY"
               className="btn mt-6 text-l bg-[#8d7352] text-white lg:ml-6 md:text-base lg:text-lg">
               CHECK AVAILABILITY
             </button>
           </form>
         </div>
       </div>
+      {showPopup && (
+        <div className="popup">
+          <div className="popup-inner">
+            <h2>{availabilityMessage}</h2>
+            <button onClick={closePopup}>Close</button>
+          </div>
+        </div>
+      )}
     </>
   );
 };
