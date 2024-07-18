b2f9cca53bd59e5b354dbcb0747046ac3e966ae1
felixDev22 <felixnzomo@gmail.com>
2024-07-19 00:09:30 +0300

    Adjust the z position for the block

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <div className="flex flex-row overflow-hidden ">
       <div className=" h-[100vh] w-[100vh] lg:bg-[#3a2d2d] lg:w-1/2 lg:h-screen lg:mt-0 lg:relative lg:z-10 p-4 md:z-50 md:mt-52 md:justify-center  md:bg-transparent flex items-center justify-center flex-col text-white">
         <div className="flex justify-center items-center ">
-          <div className="bg-[#3a2d2d] p-10 z-50 md:absolute lg:bg-transparent">
+          <div className="bg-[#3a2d2d] p-10 z-20 md:absolute lg:bg-transparent">
             <div className="w-[55vw] md:[65vw] lg:px-44">
               <h3 className=" font-script lg:text-5xl md:text-6xl text-[#e0b15a] text-3xl">
                 Experience
