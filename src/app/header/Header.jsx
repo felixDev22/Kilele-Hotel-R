3008df2fbdb518e4a4bfda0bd78e5ecc183d57c2
felixDev22 <felixnzomo@gmail.com>
2024-07-26 23:09:32 +0300

    Add background color on nav

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -9,7 +9,7 @@ import close from '../../../public/assets/close.png';
 export const Header = () => {
   const [toggle, setToggle] = useState(false);
   return (
-    <header className="border-white border-b w-full fixed top-0 left-0 px-6 py-2 lg:px-16 lg:py-2 z-50">
+    <header className="border-white border-b bg-[#3a2d2d] w-full fixed top-0 left-0 px-6 py-2 lg:px-16 lg:py-2 z-50">
       <div className="flex items-center justify-between space-x-2 md:space-x-10">
         <div>
           <Link href="/">
